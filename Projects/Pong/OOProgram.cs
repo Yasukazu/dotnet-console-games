d6f2dd7337816e1e6d423695db4d4b7a94dcd068
Yasukazu MAKINO <mkn@yskz.dev>
2022-01-05 19:07:10 +0900

    Options XML

--- Projects/Pong/OOProgram.cs
+++ Projects/Pong/OOProgram.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using CommandLineParser; // Original source code: https://github.com/wertrain/command-line-parser-cs (Version 0.1)
+﻿global using System;
+global using System.Linq;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.Threading;
+
+global using CommandLineParser; // Original source code: https://github.com/wertrain/command-line-parser-cs (Version 0.1)
 
 //    ConsoleTraceListener myWriter = new GonsoleTraceListener();
 //    Trace.Listeners.Add(myWriter);
@@ -13,11 +14,12 @@ var _rotation = 90; // Rotation.Horizontal;
 var clargs = Environment.GetCommandLineArgs();
 var pArgs = clargs[1..];
 var parseResult = Parser.Parse<Options>(pArgs);
+/*
 var speed_ratio = 1;
 var screen_width = 32;
 var screen_height = 12;
 var paddle_width = 8;
-var refresh_delay = 100;
+var delay = 100;
 var oppo_delay = 300;
 var ball_delay = 100;
 var ball_angle = 0;
@@ -30,22 +32,25 @@ if (parseResult.Tag == ParserResultType.Parsed){
 		screen_height = parseResult.Value.height;
 	if(parseResult.Value.paddle > 0)
 		paddle_width = parseResult.Value.paddle;
-	_rotation = parseResult.Value.rotation; // != 0 ? parseResult.Value.rotation : 0;
+	_rotation = parseResult.Value.rotation;
 	if(parseResult.Value.delay > 0)
-		refresh_delay = parseResult.Value.delay;
+		delay = parseResult.Value.delay;
 	if(parseResult.Value.oppo_delay > 0)
 		oppo_delay = parseResult.Value.oppo_delay;
 	if(parseResult.Value.ball_delay > 0)
 		ball_delay = parseResult.Value.ball_delay;
 	if(parseResult.Value.ball_angle != 0)
 		ball_delay = parseResult.Value.ball_angle;
-}
+} */
+// Make a new Options instance
+Options opt = parseResult.Value;
+
 Rotation rot = _rotation switch {
 	0 => Rotation.Horizontal, 90 => Rotation.Vertical,
 	_ => throw new ArgumentException("Rotation must be one of {0, 90}.")
 };
-var (screen_w, screen_h) = OnScreen.init(screen_width, screen_height);
-var game = new Game(speed_ratio, screen_w, screen_h, paddle_width, rot, refresh_delay, oppo_delay, ball_delay, ball_angle);
+(opt.width, opt.height) = OnScreen.init(opt.width, opt.height);
+var game = new Game(opt); // speed_ratio, screen_w, screen_h, paddle_width, rot, delay, oppo_delay, ball_delay, ball_angle);
 game.Run();
 public class Game {
 	public Ball Ball;
@@ -61,31 +66,38 @@ public class Game {
 	Stopwatch ballStopwatch = new();
 	TimeSpan opponentInputDelay;
 	TimeSpan ballDelay;
-	public Game(int speed_ratio, int screen_w, int screen_h, int paddleWidth, Rotation rot, 
-		int refresh_delay, int opponent_delay, int ball_delay, int ball_angle){
+	public Game(Options opt) 
+/*(int speed_ratio, int screen_w, int screen_h, int paddleWidth, 
+Rotation rot, int refresh_delay, int opponent_delay, int ball_delay, 
+int ball_angle)*/
+{
 
-		screen = new(screen_w, screen_h, rot == Rotation.Vertical ? true : false);
-		if (paddleWidth >= screen.SideToSide / 2)
-			paddleWidth = screen.SideToSide / 2;
-		selfPadl = new(range: screen.PaddleRange, width: paddleWidth, manipDict);
-		oppoPadl = new(range: screen.PaddleRange, width: paddleWidth);
+Rotation rot = opt.rotation switch {
+	0 => Rotation.Horizontal, 90 => Rotation.Vertical,
+	_ => throw new ArgumentException("Rotation must be one of {0, 90}.")
+};
+		screen = new(opt.width, opt.height, rot == Rotation.Vertical ? true : false);
+		if (opt.paddle > screen.SideToSide / 2)
+			opt.paddle = screen.SideToSide / 2;
+		selfPadl = new(range: screen.PaddleRange, width: opt.paddle, manipDict);
+		oppoPadl = new(range: screen.PaddleRange, width: opt.paddle);
 		screen.Paddles[0] = selfPadl;
 		screen.Paddles[1] = oppoPadl;
 		var ballSpec = screen.BallRanges;
-		Ball = new(ballSpec[0], ballSpec[1], StartFrom.Center, ball_angle);
+		Ball = new(ballSpec[0], ballSpec[1], StartFrom.Center, opt.ball_angle);
 		screen.Ball = Ball;
 		if (rot == Rotation.Vertical){
-			manipDict[ConsoleKey.UpArrow] = ()=>{ return selfPadl.Shift(-speed_ratio); };
-			manipDict[ConsoleKey.DownArrow] = ()=>{ return selfPadl.Shift(speed_ratio); };
+			manipDict[ConsoleKey.UpArrow] = ()=>{ return selfPadl.Shift(-opt.speed); };
+			manipDict[ConsoleKey.DownArrow] = ()=>{ return selfPadl.Shift(opt.speed); };
 		}else{
-			manipDict[ConsoleKey.LeftArrow] = ()=>{ return selfPadl.Shift(-speed_ratio); };
-			manipDict[ConsoleKey.RightArrow] = ()=>{ return selfPadl.Shift(speed_ratio); };
+			manipDict[ConsoleKey.LeftArrow] = ()=>{ return selfPadl.Shift(-opt.speed); };
+			manipDict[ConsoleKey.RightArrow] = ()=>{ return selfPadl.Shift(opt.speed); };
 		}
-		delay = TimeSpan.FromMilliseconds(refresh_delay);
-		opponentInputDelay = TimeSpan.FromMilliseconds(opponent_delay);
+		delay = TimeSpan.FromMilliseconds(opt.delay);
+		opponentInputDelay = TimeSpan.FromMilliseconds(opt.oppo_delay);
 		// Ball dX dY compensation
 		var ball_stride = Ball.Stride;
-		ballDelay = TimeSpan.FromMilliseconds(Math.Round(ball_delay / Ball.Stride));
+		ballDelay = TimeSpan.FromMilliseconds(Math.Round(opt.ball_delay / Ball.Stride));
 	// pdl = new VPaddle(screen.w, paddle_width); // NestedRange(0..(width / 3), 0..width);
 	Console.CancelKeyPress += delegate {
 		Console.CursorVisible = true;
@@ -171,26 +183,7 @@ public class Game {
 
 
 
-class Options {
-	[Option('r', "rotation", Required =false, HelpText = "rotation default 0(not rotated) and others are 90(, 180 and 270).")]
-	public int rotation { get; set;}
-	[Option('s', "speed", Required =false, HelpText = "paddle speed times default 4")]
-	public int speed { get; set;}
-	[Option('w', "width", Required =false, HelpText = "screen width default 64")]
-	public int width {get; set;}
-	[Option('h', "height", Required =false, HelpText = "screen height default 24")]
-	public int height {get; set;}
-	[Option('p', "paddle", Required =false, HelpText = "paddle width default 8")]
-	public int paddle {get; set;}
-	[Option('d', "delay", Required =false, HelpText = "ball refresh rate. default 200")]
-	public int delay {get; set;}
-	[Option('o', "opponent delay", Required =false, HelpText = "opponent delay rate. default 200")]
-	public int oppo_delay {get; set;}
-	[Option('b', "ball delay", Required =false, HelpText = "ball delay rate. default 200")]
-	public int ball_delay {get; set;}
-	[Option('a', "initial angle", Required =false, HelpText = "initial ball angle. default 0(random)")]
-	public int ball_angle {get; set;}
-}
+
 
 public class GonsoleTraceListener : ConsoleTraceListener {
 	public override void Write(string s){
