6a1c03742b3996178a0ea199664c0bfc8d8f7322
Yasukazu MAKINO <mkn@yskz.dev>
2022-01-08 15:29:31 +0900

    modify option by last games

--- Projects/Pong/OOProgram.cs
+++ Projects/Pong/OOProgram.cs
@@ -6,10 +6,11 @@ global using System.Collections.Generic;
 global using System.Diagnostics;
 global using CommandLineParser; // Original source code: https://github.com/wertrain/command-line-parser-cs (Version 0.1)
 global using System.Threading.Tasks;
+global using System.IO;
 
 // ConsoleTraceListener myWriter = new GonsoleTraceListener();
 // Trace.Listeners.Add(myWriter);
-Debug.Write("myWriter is added to Trace.Listeners.  OOProgram start.");
+// Debug.Write("myWriter is added to Trace.Listeners.  OOProgram start.");
 var _rotation = 90; // Rotation.Horizontal;
 var clargs = Environment.GetCommandLineArgs();
 var pArgs = clargs[1..];
@@ -25,6 +26,24 @@ if(new_opt != null){
 	Debug.WriteLine($"Replacing opt with new_opt.");
 	opt = new_opt;
 }
+// modify opt by last games
+var points_xml_file = @"points.xml" ;
+if(File.Exists(points_xml_file)){
+	Debug.WriteLine("Opning: " + points_xml_file);
+	Points[] pp = Points.LLoadXML(points_xml_file);
+	var selfPoints = pp.Select(x => x.Self).Sum();
+	var oppoPoints = pp.Select(x => x.Opponent).Sum();
+	if (selfPoints > oppoPoints){
+		var minus = 10;
+		Console.WriteLine("oppo delay -{minus}");
+		opt.oppo_delay -= minus;
+	}
+	else if (selfPoints < oppoPoints){
+		var plus = 50;
+		Console.WriteLine("ball delay +{plus}");
+		opt.ball_delay += plus;
+	}
+}
 
 Rotation rot = _rotation switch {
 	0 => Rotation.Horizontal, 90 => Rotation.Vertical,
@@ -35,6 +54,7 @@ Rotation rot = _rotation switch {
 const int game_repeat = 3;
 Points[] ppoints = new Points[game_repeat];
 Game game;
+Console.Write("Hit any key to start:");
 for ( int i = 0; i < game_repeat; ++i){
 	game = new Game(opt); // speed_ratio, screen_w, screen_h, paddle_width, rot, delay, oppo_delay, ball_delay, ball_angle);
 	var points = game.Run();
@@ -55,8 +75,7 @@ for ( int i = 0; i < game_repeat; ++i){
     Console.ReadKey();
 	ppoints[i] = points;
 }
-var points_xml_file = @"points.xml" ;
 Debug.WriteLine("Writing points to: " + points_xml_file);
 Points.SSaveXML(ppoints, points_xml_file);
-Console.Write("Hit any key:");
+Console.Write("Hit any key to finish:");
 Console.CursorVisible = true;
