19fcbac19c5cf0e2a6a36c85f52846a4e8428158
Yasukazu MAKINO <mkn@yskz.dev>
2022-02-12 13:06:56 +0900

    dx = cos(rad)

--- Projects/Pong/Ball.cs
+++ Projects/Pong/Ball.cs
@@ -20,6 +20,7 @@ public class Ball // : ScreenDrawItem
 	Random random = new();
 	int org_degree {get; init;}
 	public double Angle => Math.Atan2(dY, dX);
+	public float k;
 
 	/// <summary>
 	/// itself has dX and dY
@@ -30,18 +31,15 @@ public class Ball // : ScreenDrawItem
 	/// <param name="degree">random dx and dy are set if 0</param>
     public Ball(Range x_range, Range y_range, StartFrom start_from = StartFrom.Center, int degree = 0) {
         float dx, dy;
-        if (degree == 0) {
-            /* float randomFloat = (float)random.NextDouble() * 2f; dy = Math.Max(randomFloat, 1f - randomFloat);
-             dx = 1f - dy; */
-			degree = (random.Next(15) + 15) * (new int[]{1, -1}[random.Next(2)]);
-        }
-		else
-			org_degree = degree;
+        org_degree = degree;
+        /* float randomFloat = (float)random.NextDouble() * 2f; dy = Math.Max(randomFloat, 1f - randomFloat);
+		 dx = 1f - dy; */
+		degree = (org_degree == 0) ? (random.Next(15) + 15) * (new int[]{1, -1}[random.Next(2)]) : 0;
 		Debug.Write($"degree:[{degree}] of Ball.");
 		var rad = (Math.PI / 180) * degree;
-        dx = (float)Math.Sin(rad);
-        dy = (float)Math.Cos(rad);
-		var k = (float)Math.Sqrt(Math.Pow(x_range.End.Value - x_range.Start.Value, 2)
+        dx = (float)Math.Cos(rad);
+        dy = (float)Math.Sin(rad);
+		k = (float)Math.Sqrt(Math.Pow(x_range.End.Value - x_range.Start.Value, 2)
 		 + Math.Pow(y_range.End.Value - y_range.Start.Value, 2)) / 10;
 		dx *= k;
 		dy *= k;
@@ -59,18 +57,12 @@ public class Ball // : ScreenDrawItem
 
 	public void Reset() {
         float dx, dy;
-		int degree;
-        if (org_degree == 0) {
-			degree = random.Next(15) * (new int[]{1, -1}[random.Next(2)]);
-        }
-		else
-			degree = org_degree;
+		int degree = (org_degree == 0) ? (random.Next(15) + 15) * (new int[]{1, -1}[random.Next(2)]) : 0;
 		Debug.Write($"degree:[{degree}] of Ball.");
 		var rad = (Math.PI / 180) * degree;
-        dx = (float)Math.Sin(rad);
-        dy = (float)Math.Cos(rad);
-		var k = (float)Math.Sqrt(Math.Pow(XOffset.Max, 2) +
-		 Math.Pow(YOffset.Max, 2)) / 10;
+        dx = (float)Math.Cos(rad);
+        dy = (float)Math.Sin(rad);
+		// var k = (float)Math.Sqrt(Math.Pow(XOffset.Max, 2) + Math.Pow(YOffset.Max, 2)) / 10;
 		dx *= k;
 		dy *= k;
         (dX, dY) = (dx, dy);
