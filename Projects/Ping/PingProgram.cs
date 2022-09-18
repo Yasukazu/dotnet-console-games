6f05e223d49e08ef520b207e6b60bec32ef41107
YasukazuMakino <mkn@yskz.dev>
2022-09-19 08:16:55 +0900

    VirtualScreen default params

--- Projects/Ping/PingProgram.cs
+++ Projects/Ping/PingProgram.cs
@@ -5,21 +5,22 @@ global using System.Collections;
 global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.Threading.Tasks;
-global using System.IO;
 global using System.Drawing;
 
 using ping;
 using pong;
 
-int w, h;
+// int w, h;
+// (w, h) = pong.OnScreen.init();
 Console.Clear();
-(w, h) = pong.OnScreen.init();
 BallO ball = new ();
-var screen = new VirtualScreen(ball);
+var screen = new VirtualScreen();
 screen.DrawWalls();
 ball.moveTo(screen.Width / 2, screen.Height / 2);
+screen.draw();
 Debug.Print($"Ball2={ball.ToString()}");
 ball.moveBy(1.1f, 2.3f);
+screen.draw();
 ball.speedUp(0.1f, -2.4f);
 Debug.Print($"Ball2={ball.ToString()}");
 
@@ -43,8 +44,8 @@ public class VirtualScreen {
   }
   PointF lastBallPos = new();
   BallO ball;
-  public VirtualScreen (BallO ball, int width = 0, int height = 0) {
-    this.ball = ball;
+  public VirtualScreen (int width = 0, int height = 0) {
+    ball = new BallO();
     (_w, _h) = pong.OnScreen.init(width, height);
     if (_w < 4) {
       throw new ApplicationException("Screen width is not enough.");
@@ -52,8 +53,6 @@ public class VirtualScreen {
     if (_h < 4) {
       throw new ApplicationException("Screen height is not enough.");
     }
-    _w = Math.Min(_w, width);
-    _h = Math.Min(_h, height);
     screen = new Screen(_w, _h);
     window = new RectangleF(new PointF(0, 0), new SizeF(_w, _h));
     BitImage = new BitArray[_h];
@@ -62,32 +61,37 @@ public class VirtualScreen {
     }
   }
 
+  public void MoveBallTo(float x, float y) {
+    // TODO: BitImage
+    ball.moveTo(x, y);
+
+  }
+
   public void draw() {
     Char[] cc = new Char[_w];
 
+    //TODO: PlayerPaddle draw
+    //TODO: EnemyPaddle draw
     for (int i = 0; i < _h; ++i) {
-      Array.Fill(cc, ' ');
-      if (i == 0 || i == _h - 1) {
-        //TODO: Paddle draw
-      }
-      else {
-        cc[0] = cc[_w - 1] = WallChar;
         BitArray bb = (BitArray)BitImage[i].Clone();
         bb[0] = bb[_w - 1] = false;
-        var anyTrue = false;
-        for(int j = 0; j < bb.Length; ++j) {
+        int anyTrue = 0;
+        int j = 0;
+        for(j = 1; j < bb.Length - 1; ++j) {
           if (bb[j] == true) {
-            anyTrue = true;
+            anyTrue = j;
             break;
           }
         }
-        if (anyTrue) {
-
+        if (anyTrue > 0) {
+          Array.Fill(cc, ' ');
+          cc[0] = cc[_w - 1] = WallChar;
+          cc[anyTrue] = BallChar;
+          Console.SetCursorPosition(0, j);
+          Console.Write(new string(cc));
+          break;
         }
-
-
       }
-    }
   }
 
   public readonly Char SpaceChar = ' ';
